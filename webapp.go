e649c18aae51b18faf1beb01a71b067667ad11bf
OKelt <oakelton@gmail.com>
2018-12-24 23:42:34 -0800

    Fixed ServeJSON

--- webapp.go
+++ webapp.go
@@ -99,7 +99,10 @@ func MakePostHandler (fn PostHandler) func(http.ResponseWriter,
 func ServeJSON(w http.ResponseWriter, r *http.Request, item interface{}) {
 	encoder := json.NewEncoder(w)
 	err := encoder.Encode(item)
-	if err != nil { http.Error(w, r, http.StatusInternalServerError) }
+	if err != nil {
+		http.Error(w, r, http.StatusInternalServerError)
+		return
+	}
 	w.Header().Set("Content-Type", "application/json")
 }//-- end func ServeJSON
 
