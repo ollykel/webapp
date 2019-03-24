2cf429a316f073b3026c1be97ca462ae15d83fdc
OKelt <oakelton@gmail.com>
2019-03-23 20:39:08 -0700

    added "WaitSecs" to ServerConfig, DefaultServer to allow waiting before starting application; allows for synchronization with db in Docker

--- server.go
+++ server.go
@@ -9,14 +9,29 @@ package webapp
 import (
 	"net/http"
 	"context"
+	"fmt"
+	"log"
 	"errors"
+	"time"
 )
 
 type ServerConfig struct {
 	Port string
+	WaitSecs int
 	TCPEnabled bool
 	CertFile, KeyFile string
 }//-- end ServerConfig struct
+
+func (cfg *ServerConfig) Validate () error {
+	if cfg.WaitSecs < 0 {
+		return fmt.Errorf("WaitSecs must be 0 or greater (got: %d)",
+			cfg.WaitSecs)
+	}
+	if cfg.TCPEnabled && (cfg.CertFile == "" || cfg.KeyFile == "") {
+		return errors.New("TCPEnabled, but CertFile or KeyFile not given")
+	}
+	return nil
+}//-- end DefaultServer.Validate
 
 type Server interface {
 	Init (cfg *ServerConfig, handler Handler) error
@@ -29,6 +44,7 @@ type Server interface {
 // Wrapper for default net/http Server, to satisfy interface
 type DefaultServer struct {
 	http.Server
+	waitSecs int
 	tcpEnabled bool
 	certFile, keyFile string
 }//-- end DefaultServer struct
@@ -36,12 +52,11 @@ type DefaultServer struct {
 func (svr *DefaultServer) Init (cfg *ServerConfig, handler Handler) error {
 	if cfg == nil { return errors.New("passed nil ServerConfig") }
 	if handler == nil { return errors.New("passed nil Handler") }
-	if cfg.TCPEnabled && (cfg.CertFile == "" || cfg.KeyFile == "") {
-		return errors.New(
-			"TCPEnabled == true, but CertFile or KeyFile empty")
-	}
+	err := cfg.Validate()
+	if err != nil { return err }
 	svr.Addr = cfg.Port
 	svr.Handler = handler
+	svr.waitSecs = cfg.WaitSecs
 	svr.tcpEnabled = cfg.TCPEnabled
 	if cfg.TCPEnabled {
 		svr.certFile, svr.keyFile = cfg.CertFile, cfg.KeyFile
@@ -54,6 +69,10 @@ func (svr *DefaultServer) GetAddr () string {
 }//-- end func DefaultServer.GetAddr
 
 func (svr *DefaultServer) Serve () error {
+	if svr.waitSecs > 0 {
+		log.Printf("Waiting %d seconds...", svr.waitSecs)
+		time.Sleep(time.Duration(svr.waitSecs) * time.Second)
+	}
 	if svr.tcpEnabled {
 		return svr.ListenAndServeTLS(svr.certFile, svr.keyFile)
 	}
